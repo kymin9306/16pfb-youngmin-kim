3164b8d888d68c1254c0d7e8486db0cb53cdf331
CAD Client <CAD Client>
2016-04-08 15:56:43 +0900

    added file ex11, ex12, ex13.py

---
+++ ex13/ex13.py
@@ -0,0 +1,12 @@
+# **** coding utf8 ****
+# http:// learnpythonthehardway.org/book/ex13.html
+
+from sys import argv
+
+print("argv = " + str(argv))
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
