d413de06584cb5a31f9d707542d0bb127684c4fe
CAD Client <CAD Client>
2016-04-15 15:41:27 +0900

    added ex16.py

--- ex16/ex16.py
+++ ex16/ex16.py
@@ -9,7 +9,9 @@ print "If you do want that, hit RETURN."
 raw_input("?")
 
 print "Opening the file..."
-target = open(filename, 'w')print "Truncating the file.  Goodbye!"
+target = open(filename, 'w')
+
+print "Truncating the file.  Goodbye!"
 target.truncate()
 
 print "Now I'm going to ask you for three lines."
