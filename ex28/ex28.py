ac766559b223a4dba85f08c4eed2bd93d9ae0841
CAD Client <CAD Client>
2016-05-13 16:16:12 +0900

    ex28.py

---
+++ ex28/ex28.py
@@ -0,0 +1,9 @@
+# -*- coding: utf8
+# 참고문헌: http://learnpythonhardway.org/book/ex28.html
+
+print("1. true and true = %s" % str(True and True))
+print("2. false and true = %s"% str( False and True))
+print("3. 1==1 and 2==1 = %s" % ((1==1)) and (2==1))
+print("4. 'test'== 'test' = %s" %str('test'=='test'))
+print("5. 1 == 1 or 2 != 1 =%s" %(1 == 1) or (2 != 1))
+print("6.True and 1 == 1 = %s " %)
